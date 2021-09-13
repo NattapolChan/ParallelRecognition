425f5ff3d70d94209c424a150c35062f592ecfb3
Nattapol Chanpaisit <66078943+NattapolChan@users.noreply.github.com>
2021-09-13 21:18:07 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,13 +34,26 @@ def HoughTransformLine(*coord_x, *coord_y):
   H = [[0 for k in range(THETA)] for j in range(RADIUS)]
   for i in range(len(coord_x)):
     for x in range(THETA):
-      r = round((coord_x[i])*math.cos(x*math.pi/180) + (coord_y[i])*math.sin(x*math.pi/180))
+      r = round((coord_x[i])*math.cos(x*math.pi/180) + (coord_y[i])*math.sin(x*math.pi/180)) # r = xcos(angle) + ysin(angle)
       if r<RADIUS//2 - 2 and r>-RADIUS//2 + 2:
         H[r-1+RADIUS//2][x]+=1
         H[r+RADIUS//2][x]+=1
         H[r+1+RADIUS//2][x]+=1
   return H # accumulator function
       
+def HoughTransformCircle(*coord_x, *coord_y):
+  HEIGHT = 1000
+  WIDTH = 1000
+  H = [[[0 for k in range(410)] for j in range(HEIGHT)] for i in range(WIDTH)]
+  for i in range(len(coord_x)):
+    for x in range(WIDTH):
+      for y in range(HEIGHT):
+        r = math.floor(math.sqrt((coord_x[i]-x)*(coord_x[i]-x)+(coord_y[i]-y)*(coord_y[i]-y)))
+        if r<405 and r>=2 and WIDTH-x > 3 and x > 3 and y > 3 and HEIGHT - y > 3:
+          H[x][y][r+1]+=1
+          H[x][y][r]+=1
+          H[x][y][r-1]+=1
+  return H
 
 def FindArgMax(*H):
   plt.imshow(np.array(H), cmap = 'gray', aspect= 180/250)
