9fb2a83d21302ea7fbbcbe22caddca3b906c71c8
Nattapol Chanpaisit <66078943+NattapolChan@users.noreply.github.com>
2021-09-13 21:21:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ h = np.array(H)
 xmax, ymax = FindArgMax(H)
 display = CoordToImage(coord_x, coord_y)
 
+
+
+# for line
 arr = []
 for i in range(h.shape[1]):
   sub = 0
@@ -68,3 +71,48 @@ p = fig.add_subplot(1,3,1)
 p.imshow(img_2, cmap = 'binary')
 plt.xlim(0,np.array(display).shape[1])
 plt.ylim(0,np.array(display).shape[0])
+
+
+
+
+# for circle
+argmax_CM = [np.unravel_index(np.argmax(h), h.shape) for r in h]
+print(argmax_CM)
+
+arr_1 = [[0 for j in range(HEIGHT)] for i in range(WIDTH)]
+arr_3 = [[0 for j in range(HEIGHT)] for i in range(WIDTH)]
+arr_1 = np.array(arr_1)
+arr_3 = np.array(arr_3)
+for i in range(HEIGHT):
+  for j in range(WIDTH):
+    for k in range(400):
+      arr_1[i,j] += h[i,j,k+1] * h[i,j,k+1]
+      arr_3[i,j] += h[i,j,k+1]
+
+plt.imshow(edges_2)
+plt.show()
+
+a = np.sum(h*h, axis=2)[400:400 + height,400:400+width]
+argmax = [np.unravel_index(np.argmax(a), a.shape) for r in a]
+
+print(argmax)
+
+#circle
+plt.imshow(np.sum(np.array(H)*np.array(H), axis = 2)[400:,400:], cmap = 'Reds')
+plt.xlim(0,width)
+plt.ylim(height,0)
+plt.colorbar()
+plt.show()
+
+plt.imshow(np.array(edges_2), cmap = 'binary')
+plt.xlim(0,width)
+plt.ylim(height,0)
+ax = plt.gca()
+
+max_radius = 0
+for i in range(100):
+  if h[400+argmax[0][0],400+argmax[0][1],i] > h[400+argmax[0][0],400+argmax[0][1],max_radius]:
+    max_radius = i
+
+circle1 = plt.Circle((argmax[0][1], argmax[0][0]), max_radius, color = 'r', fill=False)
+ax.add_patch(circle1)
